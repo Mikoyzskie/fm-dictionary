71271551d0f280d52ed9529340bca83e0b865c67
Mikoyzskie <rapidstrike13@gmail.com>
2024-01-15 20:04:11 +0800

    formatting from the api

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,8 +9,8 @@ export default async function Home() {
     return response.json()
   }
 
-  const result = await searchWord("test")
-  console.log(result[0].meanings[0].definitions);
+  const result = await searchWord("search")
+  console.log(result[0].meanings[1]);
 
   // const audio = new Audio;
   // audio.src = "https://api.dictionaryapi.dev/media/pronunciations/en/keyboard-us.mp3"
@@ -31,28 +31,37 @@ export default async function Home() {
       <main className='mb-[124px]'>
         <div className='flex items-center justify-between mb-[42px]'>
           <div>
-            <h1 className='text-[64px] font-bold'>keyboard</h1>
-            <p className='text-[#A445ED] text-2xl '>{'/ˈkiːbɔːd/'}</p>
+            <h1 className='text-[64px] font-bold'>{result[0].word}</h1>
+            <p className='text-[#A445ED] text-2xl '>{result[0].phonetic}</p>
           </div>
 
           <svg className="hover:cursor-pointer" xmlns="http://www.w3.org/2000/svg" width="75" height="75" viewBox="0 0 75 75"><g fill="#A445ED" fillRule="evenodd"><circle cx="37.5" cy="37.5" r="37.5" opacity=".25" /><path d="M29 27v21l21-10.5z" /></g></svg>
         </div>
-        <div className='flex items-center gap-8 mb-[43px]'>
-          <span className='text-2xl'>noun</span>
-          <div className='h-[1px] bg-[#3A3A3A] w-full' />
-        </div>
-        <div>
-          <p className='text-xl text-[#757575] mb-[27px]'>Meaning</p>
-          <ul className='text-lg mb-[41px] text-justify ml-[22px] list-disc list-inside'>
-            {result[0].meanings[0].definitions.map((defs: any) => {
-              return <li key={defs.definition}>{defs.definition}</li>
-            })}
-          </ul>
-          <div className='flex gap-10 mb-[43px]'>
-            <p className='text-[#757575]'>Synonyms</p>
-            <p className='text-[#A445ED] text-xl font-bold'>electronic keyboard</p>
+        {result[0].meanings.map((defs: any) =>
+          <div key={defs.definition}>
+            <div className='flex items-center gap-8 mb-[43px]'>
+              <span className='text-2xl'>{defs.partOfSpeech}</span>
+              <div className='h-[1px] bg-[#3A3A3A] w-full' />
+            </div>
+            <div>
+              <p className='text-xl text-[#757575] mb-[27px]'>Meaning</p>
+              <ul className='text-lg mb-[41px] text-justify ml-[44px] list-disc list-outside'>
+
+                {/* {
+                  defs.definitions.map((means: any, ) => (
+                    <li key={means}><p className="pl-3">{means}</p></li>
+                  ))
+                } */}
+
+
+              </ul>
+              <div className='flex gap-10 mb-[43px]'>
+                <p className='text-[#757575]'>Synonyms</p>
+                <p className='text-[#A445ED] text-xl font-bold'>electronic keyboard</p>
+              </div>
+            </div>
           </div>
-        </div>
+        )}
         <div className='flex items-center gap-8 mb-[43px]'>
           <span className='text-2xl'>verb</span>
           <div className='h-[1px] bg-[#3A3A3A] w-full' />
@@ -73,7 +82,9 @@ export default async function Home() {
           <div className='flex items-center gap-[25px] text-sm'>
             <span className='text-[#757575]'>Source</span>
             <a href="https://en.wiktionary.org/wiki/keyboard" target='_blank' className='flex items-center gap-2'>
-              https://en.wiktionary.org/wiki/keyboard
+              {result[0].sourceUrls.map((url: string) => (
+                url
+              ))}
               <svg xmlns="http://www.w3.org/2000/svg" width="14" height="14" viewBox="0 0 14 14"><path fill="none" stroke="#838383" strokeLinecap="round" strokeLinejoin="round" strokeWidth="1.5" d="M6.09 3.545H2.456A1.455 1.455 0 0 0 1 5v6.545A1.455 1.455 0 0 0 2.455 13H9a1.455 1.455 0 0 0 1.455-1.455V7.91m-5.091.727 7.272-7.272m0 0H9m3.636 0V5" /></svg>
             </a>
           </div>
